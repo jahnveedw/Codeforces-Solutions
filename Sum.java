355db4ca65f924df89ab75754884330960413284
Jahnvee D <jahnveed@gmail.com>
2026-08-03 20:45:06 +0530

    1742A solved

---
+++ Sum.java
@@ -0,0 +1,21 @@
+//1742A
+import java.util.*;
+public class Sum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a+b==c || a+c==b || b+c==a ){
+                System.out.println("Yes");
+            }else{
+                System.out.println("NO");
+            }
+        }
+        
+        
+        sc.close();
+    }
+}
