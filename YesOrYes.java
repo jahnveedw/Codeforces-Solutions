fd96bac88c8cfb517683500cecf29d590e2c641b
Jahnvee D <jahnveed@gmail.com>
2026-08-18 22:56:22 +0530

    1703A solved

---
+++ YesOrYes.java
@@ -0,0 +1,20 @@
+//1703A
+import java.util.*;
+public class YesOrYes {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<t;i++){
+            String value=sc.nextLine();
+            if(value.toLowerCase().equals("yes")){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+
+
+        sc.close();
+    }
+}
