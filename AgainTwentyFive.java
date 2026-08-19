2fb2763f6adaaf42ae7a4ed6afeef05d77c2c4c7
Jahnvee D <jahnveed@gmail.com>
2026-08-19 22:01:12 +0530

    630A solved

---
+++ AgainTwentyFive.java
@@ -0,0 +1,11 @@
+//630A
+import java.util.Scanner;
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(25);
+
+        sc.close();
+    }
+}
