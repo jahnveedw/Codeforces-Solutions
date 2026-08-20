245a6808fb031b1bd04c9536898bc07c737c82ab
Jahnvee D <jahnveed@gmail.com>
2026-08-20 20:48:12 +0530

    630A changed

--- AgainTwentyFive.java
+++ AgainTwentyFive.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AgainTwentyFive {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
+        long n=sc.nextLong();
         System.out.println(25);
 
         sc.close();
