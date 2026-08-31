b22600761b7dedc95b90a2a50b4f646bb3942dd1
Jahnvee D <jahnveed@gmail.com>
2026-08-31 23:19:51 +0530

    1760A solved

---
+++ MediumNumber.java
@@ -0,0 +1,19 @@
+//1760A
+import java.util.*;
+public class MediumNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int[] arr={a,b,c};
+            Arrays.sort(arr);
+            System.out.println(arr[1]);
+
+        }
+
+        sc.close();
+    }
+}
