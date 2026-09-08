1371077504d441426c827d24795fcc3a6dc1e540
Jahnvee D <jahnveed@gmail.com>
2026-09-08 23:46:35 +0530

    2009A solved

---
+++ Minimize.java
@@ -0,0 +1,17 @@
+//2009A
+import java.util.*;
+public class Minimize {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(b-a);
+        }
+
+
+
+        sc.close();
+    }
+}
