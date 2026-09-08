aa8c7b22f21aac05e3cf09aed89e93c28a3651b4
Jahnvee D <jahnveed@gmail.com>
2026-09-08 23:46:07 +0530

    1850A solved

---
+++ ToMyCritics.java
@@ -0,0 +1,22 @@
+//1850A
+import java.util.Scanner;
+public class ToMyCritics {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a+b >=10 || b+c>=10 || a+c>=10){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+
+
+
+        sc.close();
+    }
+}
